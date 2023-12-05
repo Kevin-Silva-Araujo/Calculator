c023a9ddbcab5d7666553f90944e59942939d322
Kevin Silva Araujo <123kevindante@gmail.com>
2023-12-05 16:44:31 -0300

    Calculadora

---
+++ main.js
@@ -0,0 +1,46 @@
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%","*","/","-","+","="]
+let output = ""
+
+//define function to calculate based on button clicked
+const calculate = (btnValue) =>{
+    if(btnValue === "=" && output !== ""){
+        //if output has '%', replace with '/100' before evaluating
+        output = eval(output.replace("%","/100"))
+    }else if(btnValue === "AC"){
+        output = ""
+    }else if(btnValue === "DEL"){
+        //if DEL button is clicked remove the last character from the output
+        output = output.toString().slice(0,-1);
+    }else{
+        //if output is emty and button is specialChars then return
+        if(output === "" && specialChars.includes(btnValue)) return;
+        output += btnValue
+    }
+    display.value = output;
+}
+
+
+
+
+//add event listener to button call Calculate() on click
+
+buttons.forEach(buttons =>{
+    //button click listener calls calculate() with dataset value as argument
+    buttons.addEventListener("click", e => calculate(e.target.dataset.value))
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
